345fa3c22415b20deb3db38286d9303a7a63d2d1
Tine Kleivane <kleivane@gmail.com>
2023-06-13 22:14:15 +0200

    Add proxy-endpoint
    Missing: extract access token from spring security and forward

--- backend/src/main/java/no/digdir/simplifiedonboarding/TestController.java
+++ backend/src/main/java/no/digdir/simplifiedonboarding/TestController.java
@@ -1,6 +1,8 @@
 package no.digdir.simplifiedonboarding;
 
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.cloud.gateway.mvc.ProxyExchange;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -23,6 +25,11 @@ public class TestController {
     public void method(HttpServletResponse httpServletResponse) {
         httpServletResponse.setHeader("Location", "http://localhost:3000");
         httpServletResponse.setStatus(302);
+    }
+
+    @GetMapping("/proxy")
+    public ResponseEntity<?> proxy(ProxyExchange<byte[]> proxy, @AuthenticationPrincipal OAuth2User principal) throws Exception {
+        return proxy.uri("http://localhost:8181" + "/image/png").get();
     }
 
 }
